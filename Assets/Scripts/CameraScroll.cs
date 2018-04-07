d4174e661f2e84c10831a7ef2caba93956c6356b
USER-PC\User <artem.voronov90@gmail.com>
2018-04-08 00:49:32 +0300

    CameraScroll

---
+++ Assets/Scripts/CameraScroll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScroll : MonoBehaviour {
+
+    public float speed;
+    private Vector2 startPos;
+    private Camera cam;
+    private Vector2 targetPos;
+
+	void Start ()
+    {
+        cam = GetComponent<Camera>();
+        targetPos = new Vector2(transform.position.x, transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            startPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+
+            float posX = cam.ScreenToWorldPoint(Input.mousePosition).x - startPos.x;
+            float posY = cam.ScreenToWorldPoint(Input.mousePosition).y - startPos.y;
+            targetPos = new Vector2(transform.position.x - posX, transform.position.y - posY);
+        }
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetPos.x, speed * Time.deltaTime), Mathf.Lerp(transform.position.y, targetPos.y, speed * Time.deltaTime), transform.position.z);
+	}
+}
